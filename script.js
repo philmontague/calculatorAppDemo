a45cb47359f3e31e1be318434fc9379b204cc9b4
Phil <pamramfam@gmail.com>
2023-09-06 19:30:14 -0400

    Added TODO for JS

---
+++ script.js
@@ -0,0 +1,34 @@
+// const numbers = document.querySelectorAll('.number'); 
+// const operators = document.querySelectorAll('.operator'); 
+
+
+// Declare variables for DOM elements in IIFE scope
+
+
+
+// Define functions:
+// handleNumberClick()
+// handleOperatorClick()
+// handleDecimalClick()
+// handleClear()
+// evaluateExpression()
+// updateDisplay()
+
+// Add click event listeners for buttons
+
+// Store clicked values in variables:
+// currentNumber
+// previousNumber
+// currentOperator
+
+// When operator is clicked:
+// Store currentNumber to previousNumber
+// Store operator to currentOperator
+// Clear currentNumber
+
+// When equals clicked:
+// Pass previousNumber, currentNumber and currentOperator to evaluateExpression()
+// evaluateExpression performs the math operation
+// Pass result to updateDisplay() to display
+
+// Clear all on clear button click
